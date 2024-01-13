a370b4a9d9aaae38187b5d51a227c325c734e233
Lorenzo Martin del Hoyo <delhoyo.lorenzo@gmail.com>
2024-01-13 15:07:49 -0300

    stlye(navBar): improved navBar styles

--- src/app/domains/header/components/nav-bar/nav-bar.component.ts
+++ src/app/domains/header/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, ElementRef, HostListener, Renderer2, inject, signal } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon'
 import { RouterLinkWithHref, RouterLinkActive } from '@angular/router';
 
@@ -10,5 +10,18 @@ import { RouterLinkWithHref, RouterLinkActive } from '@angular/router';
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
-  
+  isBlurred = signal(false);
+
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
+
+  @HostListener('window:scroll', ['$event'])
+  handleScroll() {
+    const scrollPercentage = (window.scrollY / (document.documentElement.scrollHeight - window.innerHeight)) * 100;
+
+    if (scrollPercentage > 10) {
+      this.isBlurred.set(true);
+    } else {
+      this.isBlurred.set(false);
+    }
+  }
 }
