9d521b0c8e4dcbb5ae4afc285ee6572ba103fb8f
Lorenzo Martin del Hoyo <delhoyo.lorenzo@gmail.com>
2024-01-23 21:34:42 -0300

    feat(contact): added content to contact view

--- src/app/domains/shared/components/layout/layout.component.ts
+++ src/app/domains/shared/components/layout/layout.component.ts
@@ -3,11 +3,12 @@ import { MatIconModule } from '@angular/material/icon'
 import { RouterOutlet } from '@angular/router';
 import { LinksBarComponent } from '../../../links/links-bar/links-bar.component';
 import { NavBarComponent } from '../../../header/components/nav-bar/nav-bar.component';
+import { FooterComponent } from '../../../footer/footer/footer.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [MatIconModule, RouterOutlet, LinksBarComponent, NavBarComponent],
+  imports: [MatIconModule, RouterOutlet, LinksBarComponent, NavBarComponent, FooterComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
